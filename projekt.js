00a06ea4d23bb35145ff5aeac0866b49b46f78c8
Sarah <sarah.stichel@skola.taby.se>
2024-05-28 10:20:05 +0200

    länkar

--- projekt.js
+++ projekt.js
@@ -1,11 +1,19 @@
 //const apiKey = "959001848c85469da0d74049624a2b72";
-const apiKey = document.getElementById("apiKey").value;
 
-document.getElementById("search").addEventListener("click", function () {
+const search = function () {
+  const apiKey = document.getElementById("apiKey").value;
   const searchTerm = document.getElementById("searchBar").value;
   const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${encodeURIComponent(
     searchTerm
-  )}&apiKey=${apiKey}`;
+  )}&apiKey=${apiKey}&addRecipeInformation=true`;
+
+  // async function getRecipes(apiUrl) {
+  //   const response = await fetch(apiUrl);
+  //   const data = await response.json();
+  //   return data.results;
+  // }
+
+  // console.log(data.results);
 
   fetch(apiUrl)
     .then((response) => {
@@ -21,20 +29,38 @@ document.getElementById("search").addEventListener("click", function () {
     .catch((error) => {
       console.error("Error:", error);
     });
-});
+};
+
+document.getElementById("search").addEventListener("click", search);
+
+function createRecipeLinks(recipes) {
+  const baseUrl = "https://spoonacular.com/recipes";
+  const linkContainer = getElementById("results");
+
+  recipes.forEach((recipe) => {
+    const title = recipe.title;
+    const urlTitle = title.toLowerCase().replace(/ /g, "-");
+    const recipeId = recipe.id;
+    const recipeUrl = `${baseUrl}/${urlTitle}-${recipeId}`;
+  });
+}
+
 function displayResults(results) {
   const resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = ""; // Rensa tidigare resultat
   results.forEach((item) => {
+    console.log(item);
     const itemDiv = document.createElement("div");
     itemDiv.textContent = item.title; // Visa receptets titel
+    itemDiv.addEventListener("click", () => {
+      window.location.href = item.sourceUrl;
+    });
     resultsDiv.appendChild(itemDiv);
   });
 }
 
-document.getElementById("randomButton").addEventListener("click", function () {
-  const randomDiv = document.getElementById("randomDiv");
-  const apiUrl = `https://api.spoonacular.com/recipes/random&apiKey=${apiKey}
-  
-  `;
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    search();
+  }
 });
